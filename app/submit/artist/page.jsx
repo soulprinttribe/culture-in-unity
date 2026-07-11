c151233621d661cff722f0fbefc2bb07126bda25
soulprinttribe <soulprinttribe@gmail.com>
2026-07-10 22:37:31 -0400

    Add files via upload

---
+++ app/submit/artist/page.jsx
@@ -0,0 +1,7 @@
+import SubmitForm from "@/components/SubmitForm";
+
+export const metadata = { title: "Show Your Art - CULTURE IN UNITY" };
+
+export default function ArtistSubmitPage() {
+  return <SubmitForm role="artist" />;
+}
