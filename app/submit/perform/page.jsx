5c1074dbf05ac330ae1ac065c10e0c358ce5640e
soulprinttribe <soulprinttribe@gmail.com>
2026-07-28 17:57:18 -0400

    Add files via upload

---
+++ app/submit/perform/page.jsx
@@ -0,0 +1,7 @@
+import SubmitForm from "@/components/SubmitForm";
+
+export const metadata = { title: "Perform Your Music - CULTURE IN UNITY" };
+
+export default function PerformSubmitPage() {
+  return <SubmitForm role="perform" />;
+}
